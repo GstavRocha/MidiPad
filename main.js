edfea565c1269d9e488d36f8583b11044dc75726
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-04-05 19:52:23 -0300

    Parei no indice e como fazer eles dinamicos 2:43

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert('Olá mundo');
+function tocaSom(idAudio){
+// o id não da para ser usado ele deve ser mais generico pois o id é elemento único.
+    document.querySelector(idAudio).play();
+}
+const listaTeclas = document.querySelectorAll('.tecla');
+ var contador = 0;
+// condicao
+// depende de uma condição booleana.
+while(contador< listaTeclas.length){
+    //rotina
+    listaTeclas[contador].onclick = function (){
+        tocaSom('#som_tecla_pom');
+    }
+    contador ++;
+    console.log(contador)
+}
