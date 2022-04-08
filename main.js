c640da09cdfc8dd9df130e6b0ea01ca29bf84d88
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-04-07 22:07:20 -0300

    PAREI EM MELHORANDO O CÓDIGO

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 function tocaSom(idAudio){
-// o id não da para ser usado ele deve ser mais generico pois o id é elemento único.
-    document.querySelector(idAudio).play();
+// PAREI EM MELHORANDO O CÓDIGO
+    // o id não da para ser usado ele deve ser mais generico pois o id é elemento único.
+   const elemento = document.querySelector(idAudio);
+   if(elemento === null){
+       alert('Elemento não encontrado');
+   }else if(elemento.localName === 'audio') {
+
+       elemento.play();
+   }
+   // usar o mozila para analizar um objeto JS
 }
 const listaTeclas = document.querySelectorAll('.tecla');
- var contador = 0;
 // condicao
 // depende de uma condição booleana.
-while(contador< listaTeclas.length){
+for(let contador = 0; contador < listaTeclas.length; contador++){
     //rotina
-    listaTeclas[contador].onclick = function (){
-        tocaSom('#som_tecla_pom');
+    //Template String.
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
     }
-    contador ++;
-    console.log(contador)
+
+    //console.log(contador)
+    tecla.onkeydown = function (event){
+
+      if(event.code === 'Enter' || event.code === 'Space' ) {
+          tecla.classList.add('ativa');
+      }
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 }
